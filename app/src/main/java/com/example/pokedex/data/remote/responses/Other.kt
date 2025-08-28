5dcc885c97d43786d2fb2d019e3ec6bf1ba607da
kimiti <kimitikevin@gmail.com>
2025-08-28 09:18:45 +0300

    displays list of pokemon

--- app/src/main/java/com/example/pokedex/data/remote/responses/Other.kt
+++ app/src/main/java/com/example/pokedex/data/remote/responses/Other.kt
@@ -1,8 +1,12 @@
 package com.example.pokedex.data.remote.responses
 
+import com.google.gson.annotations.SerializedName
+
 data class Other(
-    val dream_world: DreamWorld,
+    @SerializedName("dream_world")
+    val dreamWorld: DreamWorld,
     val home: Home,
-    val official-artwork: OfficialArtwork,
+    @SerializedName("official-artwork")
+    val officialArtwork: OfficialArtwork,
     val showdown: Showdown
 )
---
+++ app/src/main/java/com/example/pokedex/pokemonlist/PokemonListViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.pokedex.pokemonlist
+
+import androidx.lifecycle.ViewModel
+import com.example.pokedex.repository.PokemonRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class PokemonListViewModel @Inject constructor(
+    private val repository: PokemonRepository
+) : ViewModel() {
+
+}
